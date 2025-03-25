aabf726ea091eedde5f9de1dc1f209111965f440
MShojaa <maziar.shojaa@gmail.com>
2025-03-25 05:54:02 +0330

    Update configs.bat

--- install-config.bat
+++ install-config.bat
@@ -1,9 +1,15 @@
 git clone https://github.com/MSH-LibCpp/configs.git
 
+:: Move directories
+xcopy /E /I /Y configs\.vscode .vscode
+xcopy /E /I /Y configs\cmake cmake
+
 :: Move files
+copy /Y configs\AskAI.md AskAI.md
+copy /Y configs\.clang-format .clang-format
 copy /Y configs\CMakePresets.json CMakePresets.json
-copy /Y configs\run-cursor.bat run-cursor.bat
 copy /Y configs\install-config.bat install-config.bat
+copy /Y configs\run-cursor.bat run-cursor.bat
 
 :: Remove the configs directory
 rmdir /S /Q configs
--- pre-config.bat
+++ pre-config.bat
@@ -5,13 +5,14 @@ xcopy /E /I /Y configs\.vscode .vscode
 xcopy /E /I /Y configs\cmake cmake
 
 :: Move files
+copy /Y configs\AskAI.md AskAI.md
 copy /Y configs\.clang-format .clang-format
-copy /Y configs\.gitignore .gitignore
 copy /Y configs\CMakePresets.json CMakePresets.json
+copy /Y configs\install-config.bat install-config.bat
 copy /Y configs\run-cursor.bat run-cursor.bat
-copy /Y configs\pre-config.bat pre-config.bat
-copy /Y configs\README.md README.md
+copy /Y configs\.gitignore .gitignore
 copy /Y configs\CMakeLists.txt CMakeLists.txt
+copy /Y configs\README.md README.md
 
 :: Force add .vscode/settings.json to git
 git add -f .vscode/settings.json
@@ -20,4 +21,7 @@ git add -f .vscode/settings.json
 rmdir /S /Q configs
 
 :: Run the cursor batch file
-run-cursor.bat
+start "" cmd /c "run-cursor.bat"
+
+:: Delete this script itself
+del "%~f0"
