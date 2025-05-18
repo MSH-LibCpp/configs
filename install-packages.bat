c7a500306c57b91cc75e29799cbab30d992dcd8d
MShojaa <maziar.shojaa@gmail.com>
2025-05-18 23:23:27 +0330

    Add log file to install-package.bat

--- install-packages.bat
+++ install-packages.bat
@@ -1,5 +1,11 @@
 @echo off
 setlocal enabledelayedexpansion
+
+:: Change working directory to script's location
+cd /d "%~dp0"
+set "LOGFILE=%~dp0install-packages.log"
+echo Log file: %LOGFILE%
+
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
 :: Optional: set MSH_ROOT_PATH if not already defined
@@ -18,63 +24,63 @@ echo Normalized MSH_ROOT_PATH: %MSH_ROOT_PATH%
 :: List of all configure/build presets
 set presets=^
 msvc-x64-win10-release ^
-msvc-x64-win10-debug ^
-msvc-x86-win10-release ^
-msvc-x86-win10-debug ^
-gxx-x64-release ^
-gxx-x64-debug ^
-gxx-x86-release ^
-gxx-x86-debug
+msvc-x64-win10-debug
+@REM msvc-x86-win10-release ^
+@REM msvc-x86-win10-debug ^
+@REM gxx-x64-release ^
+@REM gxx-x64-debug ^
+@REM gxx-x86-release ^
+@REM gxx-x86-debug
 
 :: Loop through each preset and run CMake steps
 for %%P in (%presets%) do (
-    echo.
-    echo ============================
-    echo ==== Processing %%P ====
-    echo ============================
+    echo -------- INSTALL %%P --------
 
-    echo [%%P] Configuring...
-    echo -------- CONFIGURE %%P --------
-    cmake --preset %%P
+    echo -------- CONFIGURE %%P -------- >> "%LOGFILE%" 2>&1
+    cmake --preset %%P >> "%LOGFILE%" 2>&1
     if errorlevel 1 (
         echo [ERROR] Configure step failed for %%P
         exit /b 1
     )
 
-    echo [%%P] Building...
-    echo -------- BUILD %%P --------
-    cmake --build --preset %%P
+    echo -------- BUILD %%P -------- >> "%LOGFILE%" 2>&1
+    cmake --build --preset %%P >> "%LOGFILE%" 2>&1
     if errorlevel 1 (
         echo [ERROR] Build step failed for %%P
         exit /b 1
     )
 
-    echo [%%P] Installing...
-    echo -------- INSTALL %%P --------
+    echo -------- INSTALL %%P -------- >> "%LOGFILE%" 2>&1
     set "buildDir=build/%%P"
-    cmake --install !buildDir!
+    cmake --install !buildDir! >> "%LOGFILE%" 2>&1
     if errorlevel 1 (
         echo [ERROR] Install step failed for %%P
         exit /b 1
     )
 
-    echo [%%P] Cleaning up build directory: !buildDir!
+    echo [%%P] Cleaning up build directory: !buildDir! >> "%LOGFILE%" 2>&1
     rmdir /s /q "!buildDir!"
     if exist "!buildDir!" (
         echo [%%P] Failed to remove directory.
+        echo [%%P] Failed to remove directory. >> "%LOGFILE%" 2>&1
     ) else (
-        echo [%%P] Build directory removed.
+        echo [%%P] Build directory removed. >> "%LOGFILE%" 2>&1
     )
 
     echo [%%P] Done!
+    echo [%%P] Done! >> "%LOGFILE%" 2>&1
 )
-
-git reset --hard
-git clean -fd
 
 echo.
 echo All builds and installs completed.
+echo. >> "%LOGFILE%" 2>&1
+echo All builds and installs completed. >> "%LOGFILE%" 2>&1
+
 endlocal
+
+@REM git reset --hard
+@REM git clean -fd
+
 exit /b 0
 
 :: Or use the old way
