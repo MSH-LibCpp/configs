297416153204e0b77e4ed508b8236e57aa186b22
MShojaa <maziar.shojaa@gmail.com>
2025-05-23 22:52:20 +0330

    Merge update-config.bat to pre-config.bat rename install-packages.bat to install.bat

--- config.bat
+++ config.bat
@@ -7,18 +7,19 @@ xcopy /E /I /Y configs\.vscode .vscode
 copy /Y configs\AskAI.md AskAI.md
 copy /Y configs\.clang-format .clang-format
 copy /Y configs\CMakePresets.json CMakePresets.json
-copy /Y configs\update-config.bat update-config.bat
-copy /Y configs\install-packages.bat install-packages.bat
+copy /Y configs\config.bat config.bat
+copy /Y configs\install.bat install.bat
 copy /Y configs\run-vscode.bat run-vscode.bat
-copy /Y configs\.gitignore .gitignore
-copy /Y configs\CMakeLists.txt CMakeLists.txt
-copy /Y configs\README.md README.md
 
-:: Force add .vscode/settings.json to git
-git add -f .vscode/settings.json
+if not exist "CMakeLists.txt" (
+  :: Move files
+  copy /Y configs\README.md README.md
+  copy /Y configs\CMakeLists.txt CMakeLists.txt
+  copy /Y configs\.gitignore .gitignore
+  
+  :: Force add .vscode/settings.json to git
+  git add -f .vscode/settings.json
+)
 
 :: Remove the configs directory
 rmdir /S /Q configs
-
-:: Delete this script itself
-del "%~f0"
